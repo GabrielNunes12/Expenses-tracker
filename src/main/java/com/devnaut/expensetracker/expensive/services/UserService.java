b3286d2582d2e1d5e8b39ec8e1cdb1eb67dbdfc4
Gabriel <prismaticnetwork12@gmail.com>
2023-10-12 19:55:23 -0300

    feat: adding password hashing

--- src/main/java/com/devnaut/expensetracker/expensive/services/UserService.java
+++ src/main/java/com/devnaut/expensetracker/expensive/services/UserService.java
@@ -1,5 +1,6 @@
 package com.devnaut.expensetracker.expensive.services;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import com.devnaut.expensetracker.expensive.dtos.UserDTO;
 import com.devnaut.expensetracker.expensive.models.User;
 import com.devnaut.expensetracker.expensive.repositories.UserRepository;
@@ -25,7 +26,7 @@ public class UserService {
     if(userRepository.findByLogin(user.getUsername())) return "User created already!";
     User newUser = new User();
     newUser.setAuthority(user.getUserEnum());
-    newUser.setPassword(user.getPassword());
+    newUser.setPassword(BCrypt.withDefaults().hashToString(4, user.getPassword().toCharArray()));
     newUser.setUsername(user.getUsername());
     userRepository.save(newUser);
     return "User created";
